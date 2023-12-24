87ebd45785396c21d2c2aa7655784743f2f07c55
nanukashubladze <123475291+nanukashubladze@users.noreply.github.com>
2023-12-24 16:22:29 +0400

    done

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("test")
+let timeout;
+
+function showCoordinates(event) {
+    console.clear();
+    console.log(`X=${event.clientX}, Y=${event.clientY}`);
+};
+
+function handleMouseMove(event) {
+    clearTimeout(timeout);
+
+    timeout = setTimeout(() => {
+        showCoordinates(event);
+    }, 1000); 
+};
+
+document.addEventListener('mousemove', handleMouseMove);
